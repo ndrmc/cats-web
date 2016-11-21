0455809d216d8b80a328f9a6bc808b7655199515
Yared Ayalew <yareda@gmail.com>
2016-11-21 13:49:28 +0300

    Add rolify role management

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  rolify
+  after_create :assign_default_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def assign_default_role
+    self.add_role(:guest) if self.roles.blank?
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121061437) do
+ActiveRecord::Schema.define(version: 20161121090258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -492,6 +492,20 @@ ActiveRecord::Schema.define(version: 20161121061437) do
     t.index ["requisition_no"], name: "index_requisitions_on_requisition_no", unique: true, using: :btree
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "created_by"
+    t.integer  "modified_by"
+    t.string   "resource_type"
+    t.integer  "resource_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_roles_on_deleted_at", using: :btree
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+    t.index ["name"], name: "index_roles_on_name", using: :btree
+  end
+
   create_table "seasons", force: :cascade do |t|
     t.string   "name",        null: false
     t.string   "description"
@@ -704,6 +718,16 @@ ActiveRecord::Schema.define(version: 20161121061437) do
     t.index ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer  "created_by"
+    t.integer  "modified_by"
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_users_roles_on_deleted_at", using: :btree
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
   end
 
 end
