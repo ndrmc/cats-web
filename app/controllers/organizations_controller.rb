e61a6d5138da4879f429be7931f529e5c804ae48
getbez <getbez@gmail.com>
2016-11-28 16:47:29 +0300

    organizations show fix

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -43,7 +43,7 @@ class OrganizationsController < ApplicationController
   def update
     respond_to do |format|
       if @organization.update(organization_params)
-        format.html { redirect_to @organization, notice: 'Organization was successfully updated.' }
+        format.html { redirect_to organizations_path, notice: 'Organization was successfully updated.' }
         format.json { render :show, status: :ok, location: @organization }
       else
         format.html { render :edit }
