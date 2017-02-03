6f265f8f675884d8dcc6f1d3823c914a99175fa6
Alexander Suraphel <alexsuraphel@gmail.com>
2017-02-03 11:26:47 +0300

    Re-annotate

--- app/models/commodity_source.rb
+++ app/models/commodity_source.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: commodity_sources
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  created_by  :integer
+#  modified_by :integer
+#  deleted     :boolean          default(FALSE)
+#  deleted_at  :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class CommoditySource < ApplicationRecord
 end
--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -2,15 +2,16 @@
 #
 # Table name: stores
 #
-#  id           :integer          not null, primary key
-#  name         :string           not null
-#  temporary    :boolean
-#  warehouse_id :integer
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  created_by   :integer
-#  modified_by  :integer
-#  deleted_at   :datetime
+#  id                :integer          not null, primary key
+#  name              :string           not null
+#  temporary         :boolean
+#  warehouse_id      :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  created_by        :integer
+#  modified_by       :integer
+#  deleted_at        :datetime
+#  store_keeper_name :string
 #
 
 require 'test_helper'
