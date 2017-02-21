316b065f51003081f6a8c08e11e4618f268002f6
dawit <dawitdb@yahoo.com>
2017-02-21 19:42:17 +0300

    add policy to user controller actions

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -12,4 +12,24 @@ def index?
     @current_user.has_role? :admin
 end
 
+def new?
+     @current_user.has_role? :admin
+end
+
+def roles
+     @current_user.has_role? :admin
+end
+
+def create
+     @current_user.has_role? :admin
+end
+
+def update
+    @current_user.has_role? :admin
+end
+
+def updateRoles
+    @current_user.has_role? :admin
+end
+
 end
