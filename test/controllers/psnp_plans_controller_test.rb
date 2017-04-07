6e6d7bbdceda00df7f14c6e198c268e0efcc4ab2
dawit <dawitdb@yahoo.com>
2017-04-07 13:17:33 +0300

    add PSNP_PLAN test

--- test/controllers/psnp_plans_controller_test.rb
+++ test/controllers/psnp_plans_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PsnpPlansControllerTest < ActionDispatch::IntegrationTest
+
+  include Devise::Test::IntegrationHelpers
+
   setup do
+     sign_in users(:admin)
     @psnp_plan = psnp_plans(:one)
   end
 
@@ -17,31 +21,34 @@ class PsnpPlansControllerTest < ActionDispatch::IntegrationTest
 
   test "should create psnp_plan" do
     assert_difference('PsnpPlan.count') do
-      post psnp_plans_url, params: { psnp_plan: {  } }
+      post psnp_plans_url, params: { psnp_plan: { 
+         year_ec: '2009', year_gc: '2017', month_from: 1, ration_id: 1, duration: 6
+       } }
     end
 
-    assert_redirected_to psnp_plan_url(PsnpPlan.last)
+    assert_redirected_to psnp_plans_url
   end
 
   test "should show psnp_plan" do
-    get psnp_plan_url(@psnp_plan)
+    get psnp_plans_url
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_psnp_plan_url(@psnp_plan)
+    get edit_psnp_plan_url(id: @psnp_plan.id)
     assert_response :success
   end
 
   test "should update psnp_plan" do
-    patch psnp_plan_url(@psnp_plan), params: { psnp_plan: {  } }
-    assert_redirected_to psnp_plan_url(@psnp_plan)
+    patch psnp_plan_url(id: @psnp_plan.id), params: { psnp_plan: {
+       year_ec: '2009', year_gc: '2017', month_from: 1,ration_id: 1, duration: 6
+      } }
+    assert_redirected_to psnp_plans_url
   end
 
   test "should destroy psnp_plan" do
-    assert_difference('PsnpPlan.count', -1) do
-      delete psnp_plan_url(@psnp_plan)
-    end
+     get "/psnp_plans/archive/#{@psnp_plan.id}"
+    assert_equal(@psnp_plan.status.to_i, PsnpPlan.statuses[:archived])
 
     assert_redirected_to psnp_plans_url
   end
