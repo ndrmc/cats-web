f51d763fac8088a310b38b0f57bdf4f75c847cd3
Yared Ayalew <yareda@gmail.com>
2017-04-19 16:09:42 +0300

    Rename usertype graphql schema to useraccounttype

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :log_level,     :debug
 set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
