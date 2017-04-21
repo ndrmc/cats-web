a22b6cda26b5c714e2160384f68e2fc68dd32909
Yared Ayalew <yareda@gmail.com>
2017-04-21 11:05:56 +0300

    Remove thread unsafe method from application_controller to get current user for models

--- app/controllers/commodity_categories_controller.rb
+++ app/controllers/commodity_categories_controller.rb
@@ -26,6 +26,7 @@ class CommodityCategoriesController < ApplicationController
   # POST /commodity_categories.json
   def create
     @commodity_category = CommodityCategory.new(commodity_category_params)
+    @commodity_category.created_by = current_user.id
 
     respond_to do |format|
       if @commodity_category.save
@@ -42,6 +43,7 @@ class CommodityCategoriesController < ApplicationController
   # PATCH/PUT /commodity_categories/1.json
   def update
     respond_to do |format|
+      @commodity_category.updated_by = current_user.id
       if @commodity_category.update(commodity_category_params)
         format.html { redirect_to commodity_categories_path, notice: 'Commodity category was successfully updated.' }
         format.json { render :show, status: :ok, location: @commodity_category }
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,14 +2,14 @@ class ApplicationRecord < ActiveRecord::Base
   acts_as_paranoid
   self.abstract_class = true
 
-  before_create :update_created_by
-  before_update :update_modified_by
+  # before_create :update_created_by
+  # before_update :update_modified_by
 
-  def update_created_by
-    self.modified_by = self.created_by = Current.user ? Current.user.id : nil
-  end
+  # def update_created_by
+  #   self.modified_by = self.created_by = Current.user ? Current.user.id : nil
+  # end
 
-  def update_modified_by
-    self.modified_by = Current.user ? Current.user.id : nil
-  end
+  # def update_modified_by
+  #   self.modified_by = Current.user ? Current.user.id : nil
+  # end
 end
