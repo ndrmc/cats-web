22905b825c2ece709a630cf516a6586cfec46388
Beza Getachew <getbez@gmail.com>
2017-05-01 09:00:11 +0300

    gin import script added

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -29,7 +29,7 @@ class Delivery < ApplicationRecord
   scope :gin_number, ->(gin_number) { where gin_number: gin_number }
   scope :fdp_id, ->(fdp_id) { where fdp_id: fdp_id }
   scope :operation_id, ->(operation_id) { where operation_id: operation_id }
-  scope :region_id, ->(region_id) {where Location.find(region_id).children.exists?(self)}
+  scope :region, ->(region_id) {where Location.find_by_name(region).children.exists?(self)}
 
   has_many :delivery_details
   accepts_nested_attributes_for :delivery_details, reject_if: :all_blank
