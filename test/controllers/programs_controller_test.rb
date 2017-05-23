7bc228c046698d46f72f0b6d8e2edc53d0d74c7c
dawit <dawitdb@yahoo.com>
2017-05-23 12:10:39 +0300

    fix CATS-1619

---
+++ test/controllers/programs_controller_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class ProgramsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:admin)
+
+    @program = programs(:one)
+  end
+
+  test "should get index" do
+    get programs_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_program_url
+    assert_response :success
+  end
+
+  test "should create program" do
+    assert_difference('Program.count') do
+      post programs_url, params: { program: { name: 'new program', code: 'np'  } }
+    end
+
+    assert_redirected_to programs_url
+  end
+
+  test "should show program" do
+    get programs_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_program_url('en',@program)
+    assert_response :success
+  end
+
+  test "should update program" do
+    patch program_url('en', @program), params: { program: { name: 'update program', code: 'up'  } }
+    assert_redirected_to program_url(@program)
+  end
+
+  test "should destroy program" do
+    assert_difference('Program.count', -1) do
+      delete program_url('en',@program)
+    end
+
+    assert_redirected_to programs_url
+  end
+end
