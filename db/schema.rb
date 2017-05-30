6443df7f8b1fd21feb7e898d18c3a57a0009e356
Yared Ayalew <yareda@gmail.com>
2017-05-30 10:09:58 +0300

    Enable state saving on datatables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529081913) do
+ActiveRecord::Schema.define(version: 20170530055949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -210,8 +210,8 @@ ActiveRecord::Schema.define(version: 20170529081913) do
     t.datetime "deleted_at"
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
-    t.string   "delivery_id_guid"
     t.string   "received_date_ec"
+    t.index ["gin_number"], name: "index_deliveries_on_gin_number", unique: true, using: :btree
     t.index ["receiving_number"], name: "index_deliveries_on_receiving_number", unique: true, using: :btree
   end
 
@@ -223,11 +223,10 @@ ActiveRecord::Schema.define(version: 20170529081913) do
     t.integer  "delivery_id"
     t.integer  "created_by"
     t.integer  "modified_by"
-    t.boolean  "deleted",              default: false
+    t.boolean  "deleted",           default: false
     t.datetime "deleted_at"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
-    t.string   "guid_ref_delivery_id"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   create_table "delivery_imports", force: :cascade do |t|
@@ -308,9 +307,7 @@ ActiveRecord::Schema.define(version: 20170529081913) do
     t.datetime "deleted_at"
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
-    t.string   "guid_ref"
     t.integer  "organization_id"
-    t.integer  "uom_id"
     t.integer  "unit_of_measure_id"
     t.index ["commodity_category_id"], name: "index_dispatch_items_on_commodity_category_id", using: :btree
     t.index ["commodity_id"], name: "index_dispatch_items_on_commodity_id", using: :btree
@@ -341,7 +338,6 @@ ActiveRecord::Schema.define(version: 20170529081913) do
     t.integer  "hub_id"
     t.integer  "warehouse_id"
     t.string   "storekeeper_name",            limit: 200,                 null: false
-    t.string   "dispatch_id_guid"
     t.string   "dispatched_date_ec"
     t.index ["fdp_id"], name: "index_dispatches_on_fdp_id", using: :btree
     t.index ["hub_id"], name: "index_dispatches_on_hub_id", using: :btree
@@ -769,7 +765,7 @@ ActiveRecord::Schema.define(version: 20170529081913) do
   create_table "projects", force: :cascade do |t|
     t.string   "project_code"
     t.integer  "commodity_id"
-    t.integer  "commodity_source"
+    t.integer  "commodity_source_id"
     t.integer  "organization_id"
     t.decimal  "amount"
     t.integer  "unit_of_measure_id"
@@ -783,7 +779,7 @@ ActiveRecord::Schema.define(version: 20170529081913) do
     t.string   "reference_no"
     t.integer  "si_id"
     t.text     "si_value"
-    t.boolean  "draft",              default: false
+    t.boolean  "draft",               default: false
     t.index ["project_code"], name: "index_projects_on_project_code", using: :btree
   end
 
