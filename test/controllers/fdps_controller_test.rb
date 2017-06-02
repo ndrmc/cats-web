5e93ce58f52c122aa896bf57b7d5169a85ba7749
dawit <dawitdb@yahoo.com>
2017-06-02 12:39:35 +0300

    fix fdp test

---
+++ test/controllers/fdps_controller_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+
+class FdpsControllerTest < ActionDispatch::IntegrationTest
+ include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:admin)
+    @fdp = fdps(:fdp1)
+  end
+
+  test "should get index" do
+    get fdps_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_fdp_url
+    assert_response :success
+  end
+
+  test "should create fdp" do
+    assert_difference('Fdp.count') do
+      post fdps_url, params: { fdp: { name: @fdp.name, zone: @fdp.zone, region: @fdp.region, location_id: @fdp.location_id } }
+    end
+
+    assert_redirected_to fdps_url
+  end
+
+  test "should show fdp" do
+    get fdp_url('en',@fdp)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_fdp_url('en',@fdp)
+    assert_response :success
+  end
+
+  test "should update fdp" do
+    patch fdp_url('en',@fdp), params: { fdp: { name: @fdp.name, zone: @fdp.zone, region: @region, location: @fdp.location  } }
+    assert_response :success
+  end
+
+  test "should destroy fdp" do
+    assert_difference('Fdp.count', -1) do
+      delete fdp_url('en',@fdp)
+    end
+
+    assert_redirected_to fdps_url
+  end
+
+  test "fdp name , zone and region must not be blank" do
+    new_fdp=Fdp.new(name: ' ', zone: ' ', region: ' ')
+    assert !new_fdp.valid?
+
+  end
+  
+  test "fdp name , zone and region must not be nil" do
+    new_fdp=Fdp.new(name: nil, zone: nil, region: nil)
+    assert !new_fdp.valid?
+
+  end
+  
+end
