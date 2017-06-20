2c9555525e92ec5cd514d00c24b7fa2f3bf66aa8
Nathnael Getahun <contact.nathnael@gmail.com>
2017-06-20 17:49:47 +0300

    CATS-1576

--- test/controllers/stock_status_controller_test.rb
+++ test/controllers/stock_status_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class StockStatusControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get stock_status_index_url
-    assert_response :success
-  end
+  
 
 end
