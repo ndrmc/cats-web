23fe6da55d8438aec7f6f5ccb05041a5044950dc
Nathnael Getahun <contact.nathnael@gmail.com>
2017-07-03 16:16:54 +0300

    CATS-1576 Started re-designing stock status page

--- app/controllers/stock_status_controller.rb
+++ app/controllers/stock_status_controller.rb
@@ -1,7 +1,11 @@
 class StockStatusController < ApplicationController
   
-  def index
+  def index2
 	@stock_status = Reports.new.journal_reports(params[:hub], params[:program], params[:commodity], params[:operation])
+  end
+
+  def index
+	@stock_status = Reports.new.stock_status_reports(params[:hub], params[:program], params[:commodity])
   end
 
 end
