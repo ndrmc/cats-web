0fea1e14acca45f82194a0c4392d8f5eabcf3fc6
dawit <dawitdb@yahoo.com>
2017-07-06 11:06:59 +0300

    change migration

--- db/migrate/20170705125617_change_data_type_quantity_for_receipt_lines.rb
+++ db/migrate/20170705125617_change_data_type_quantity_for_receipt_lines.rb
@@ -1,5 +1,15 @@
 class ChangeDataTypeQuantityForReceiptLines < ActiveRecord::Migration[5.0]
-  def change
-       change_column :receipt_lines ,:quantity ,:decimal , precision: 15, scale: 2
+ 
+  def self.up
+    change_table :receipt_lines do |t| 
+      t.change :quantity, :decimal, precision:15, scale: 2
+      end
   end
+   
+  def self.down
+    change_table :receipt_lines do|t|
+      t.change :quantity, :integer
+  end
+end
+
 end
