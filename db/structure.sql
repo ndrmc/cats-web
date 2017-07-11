fac0bc54e6537c533cc30483f0029ae88e677869
Nathnael Getahun <contact.nathnael@gmail.com>
2017-07-11 16:54:27 +0300

    CATS-1577-redone removed unsupported attribs from structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -6,12 +6,10 @@
 -- Dumped by pg_dump version 9.5.7
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
