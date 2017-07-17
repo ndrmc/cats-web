e49321dd204d70f2cd640322b2cb1dfef61df765
Nathnael Getahun <contact.nathnael@gmail.com>
2017-07-17 20:35:44 +0300

    CATS-1577 0 Fixed minor bugs in operation_loggable

--- app/models/operation_loggable.rb
+++ app/models/operation_loggable.rb
@@ -5,13 +5,13 @@ module OperationLoggable
 	      logger.info "Log operation information for Requisition"
 	      create_log_requisition
 	    elsif(self.is_a?(Dispatch))
-	      logger.info "Log operation information for Requisition"
+	      logger.info "Log operation information for Dispatch"
 	      create_log_dispatch
 	    elsif(self.is_a?(Delivery))
-	      logger.info "Log operation information for Requisition"
+	      logger.info "Log operation information for Delivery"
 	      create_log_delivery
 	    elsif(self.is_a?(Distribution))
-	      logger.info "Log operation information for Requisition"
+	      logger.info "Log operation information for Distribution"
 	      create_log_distribution
 	    end
 	end 
@@ -19,7 +19,7 @@ module OperationLoggable
 	def create_log_requisition
 		self.requisition_items.each do |requisition_item|
 			existing_operation_log = FdpOperationsLog.where(operation_id: self.operation_id, fdp_id: requisition_item.fdp_id, requisition_id: self.id, commodity_id: self.commodity_id).first
-			if( existing_operation_log.empty? )
+			if( ! existing_operation_log.present? )
 	  			fdp_op_log = FdpOperationsLog.new
 				fdp_op_log.operation_id = self.operation_id
 				fdp_op_log.fdp_id = requisition_item.fdp_id
@@ -29,7 +29,11 @@ module OperationLoggable
 				fdp_op_log.allocated_in_mt = requisition_item.amount
 				fdp_op_log.save
 			else
-				existing_operation_log.allocated_in_mt += requisition_item.amount
+				if(existing_operation_log.allocated_in_mt.blank?)
+					existing_operation_log.allocated_in_mt = requisition_item.amount
+				else
+					existing_operation_log.allocated_in_mt += requisition_item.amount
+				end					
 				existing_operation_log.save
 			end
   		end  		
@@ -40,7 +44,7 @@ module OperationLoggable
 			@requisition_obj = Requisition.where(:requisition_no => self.requisition_number).first
 			existing_operation_log = FdpOperationsLog.where(operation_id: self.operation_id, fdp_id: self.fdp_id, requisition_id: @requisition_obj.id, commodity_id: dispatch_item.commodity_id).first
 			@amount_in_ref = UnitOfMeasure.find(dispatch_item.unit_of_measure_id).to_ref(dispatch_item.quantity)		  		
-			if( existing_operation_log.empty? )
+			if( ! existing_operation_log.present? )
 	  			fdp_op_log = FdpOperationsLog.new
 				fdp_op_log.operation_id = self.operation_id
 				fdp_op_log.fdp_id = self.fdp_id
@@ -50,7 +54,11 @@ module OperationLoggable
 				fdp_op_log.dispatched_in_mt = @amount_in_ref
 				fdp_op_log.save
 			else
-				existing_operation_log.dispatched_in_mt += @amount_in_ref
+				if(existing_operation_log.dispatched_in_mt.blank?)
+					existing_operation_log.dispatched_in_mt = @amount_in_ref
+				else
+					existing_operation_log.dispatched_in_mt += @amount_in_ref
+				end				
 				existing_operation_log.save
 			end
   		end
@@ -61,7 +69,7 @@ module OperationLoggable
 			@requisition_obj = Requisition.where(:requisition_no => self.requisition_number).first
 			existing_operation_log = FdpOperationsLog.where(operation_id: self.operation_id, fdp_id: self.fdp_id, requisition_id: @requisition_obj.id, commodity_id: delivery_detail.commodity_id).first
 			@amount_in_ref = UnitOfMeasure.find(delivery_detail.uom_id).to_ref(delivery_detail.received_quantity)		  		
-			if( existing_operation_log.empty? )
+			if( ! existing_operation_log.present? )
 	  			fdp_op_log = FdpOperationsLog.new
 				fdp_op_log.operation_id = self.operation_id
 				fdp_op_log.fdp_id = self.fdp_id
@@ -71,7 +79,11 @@ module OperationLoggable
 				fdp_op_log.delivered_in_mt = @amount_in_ref
 				fdp_op_log.save
 			else
-				existing_operation_log.delivered_in_mt += @amount_in_ref
+				if(existing_operation_log.delivered_in_mt.blank?)
+					existing_operation_log.delivered_in_mt = @amount_in_ref
+				else
+					existing_operation_log.delivered_in_mt += @amount_in_ref
+				end
 				existing_operation_log.save
 			end
   		end
