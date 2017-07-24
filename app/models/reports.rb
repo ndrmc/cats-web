d7aa2161c967183bc2e444c028941e24b51a97ac
Nathnael Getahun <contact.nathnael@gmail.com>
2017-07-24 15:08:36 +0300

    CATS-1577 Fixed a failed test missing a deprecated model DispatchSummaryByFdp

--- app/models/reports.rb
+++ app/models/reports.rb
@@ -89,40 +89,34 @@ class Reports
 
 	#### Dispatch Reports - Begin #######################################################
 		def dispatch_reports_by_region operation
-			@result = DispatchSummaryByFdp.select("region_id AS location_id, region_name AS location, 
-				allo_op_id AS operation, SUM(allocated) AS allocated, SUM(dispatched) AS dispatched")
-		    .group('region_id, region_name, allo_op_id')
-		    .where('allo_op_id = ' + operation.to_s)
-
-		    @result = FdpOperationsLog.select("region_id AS location_id, region_name AS location, 
-				allo_op_id AS operation, SUM(allocated) AS allocated, SUM(dispatched) AS dispatched")
-		    .group('region_id, region_name, allo_op_id')
-		    .where('allo_op_id = ' + operation.to_s)
-			
+			@result = FdpOperationsLog.select("region_id AS location_id, region_name AS location, 
+				operation_id AS operation, SUM(allocated_in_mt) AS allocated, SUM(dispatched_in_mt) AS dispatched")
+		    .group('region_id, region_name, operation_id')
+		    .where('operation_id = ' + operation.to_s)			
 		end
 
 		def dispatch_reports_by_zone operation, region
-			@result = DispatchSummaryByFdp.select("zone_id AS location_id, zone_name AS location, 
-				allo_op_id AS operation_id, allo_req_no as requisition, commodity_name AS commodity, 
-				SUM(allocated) AS allocated, SUM(dispatched) AS dispatched")
-		    .group('zone_id, zone_name, allo_op_id, allo_req_no, commodity_name')
-		    .where('allo_op_id = ' + operation.to_s + ' AND region_id = ' + region.to_s)
+			@result = FdpOperationsLog.select("zone_id AS location_id, zone_name AS location, 
+				operation_id, requisition_no as requisition, commodity_name AS commodity, 
+				SUM(allocated_in_mt) AS allocated, SUM(dispatched_in_mt) AS dispatched")
+		    .group('zone_id, zone_name, operation_id, requisition_no, commodity_name')
+		    .where('operation_id = ' + operation.to_s + ' AND region_id = ' + region.to_s)
 		end
 
 		def dispatch_reports_by_woreda operation, zone
-			@result = DispatchSummaryByFdp.select("woreda_id AS location_id, woreda_name AS location, 
-				allo_op_id AS operation_id, allo_req_no as requisition, commodity_name AS commodity, 
-				SUM(allocated) AS allocated, SUM(dispatched) AS dispatched")
-		    .group('woreda_id, woreda_name, allo_op_id, allo_req_no, commodity_name')
-		    .where('allo_op_id = ' + operation.to_s + ' AND zone_id = ' + zone.to_s)
+			@result = FdpOperationsLog.select("woreda_id AS location_id, woreda_name AS location, 
+				operation_id, requisition_no as requisition, commodity_name AS commodity, 
+				SUM(allocated_in_mt) AS allocated, SUM(dispatched_in_mt) AS dispatched")
+		    .group('woreda_id, woreda_name, operation_id, requisition_no, commodity_name')
+		    .where('operation_id = ' + operation.to_s + ' AND zone_id = ' + zone.to_s)
 		end	
 
 		def dispatch_reports_by_fdp operation, woreda
-			@result = DispatchSummaryByFdp.select("allo_fdp AS location_id, fdp_name AS location, 
-				allo_op_id AS operation_id, allo_req_no as requisition, commodity_name AS commodity, 
-				SUM(allocated) AS allocated, SUM(dispatched) AS dispatched")
-		    .group('allo_fdp, fdp_name, allo_op_id, allo_req_no, commodity_name')
-		    .where('allo_op_id = ' + operation.to_s + ' AND woreda_id = ' + woreda.to_s)
+			@result = FdpOperationsLog.select("fdp_id AS location_id, fdp_name AS location, 
+				operation_id, requisition_no as requisition, commodity_name AS commodity, 
+				SUM(allocated_in_mt) AS allocated, SUM(dispatched_in_mt) AS dispatched")
+		    .group('fdp_id, fdp_name, operation_id, requisition_no, commodity_name')
+		    .where('operation_id = ' + operation.to_s + ' AND woreda_id = ' + woreda.to_s)
 
 		end		
 
