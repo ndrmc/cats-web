8d497a8200d125d4a887339fb414d23661b31e98
Nathnael Getahun <contact.nathnael@gmail.com>
2017-07-24 14:34:26 +0300

    CATS-1577 Database migration and rake tasks to prepare fdp_operations_logs table

--- db/migrate/20170713083617_create_fdp_operations_logs.rb
+++ db/migrate/20170713083617_create_fdp_operations_logs.rb
@@ -2,28 +2,27 @@ class CreateFdpOperationsLogs < ActiveRecord::Migration[5.0]
   def change
     create_table :fdp_operations_logs do |t|
 
-		t.references :operation
-
-    	t.references :fdp
-
-    	t.references :location
-
-    	t.references :requisition
-
-    	t.references :commodity
-
-    	t.decimal :allocated_in_mt
-
-    	t.decimal :dispatched_in_mt
-
-    	t.decimal :delivered_in_mt
-
-    	t.decimal :distributed_in_mt
-
+      t.integer :operation_id
+      t.string :operation_name
+      t.integer :fdp_id
+      t.string :fdp_name
+      t.integer :woreda_id
+      t.string :woreda_name
+      t.integer :zone_id
+      t.string :zone_name
+      t.integer :region_id
+      t.string :region_name
+      t.integer :requisition_id
+      t.string :requisition_no
+      t.integer :commodity_id
+      t.string :commodity_name
+      t.decimal :allocated_in_mt
+      t.decimal :dispatched_in_mt
+      t.decimal :delivered_in_mt
+      t.decimal :distributed_in_mt
       t.boolean :deleted, :default => false
       t.datetime :deleted_at
-
-    	t.timestamps
+      t.timestamps
     end
   end
 end
