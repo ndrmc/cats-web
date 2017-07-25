c7c3b2f293ef5a2285528339cb1084da3e654b3e
Nathnael Getahun <contact.nathnael@gmail.com>
2017-07-25 11:04:59 +0300

    CATS-1577 Set foreignkey constraint to requisition_items and unit_of_measures

--- db/migrate/20170721075445_add_column_unit_of_measure_for_requisition_item.rb
+++ db/migrate/20170721075445_add_column_unit_of_measure_for_requisition_item.rb
@@ -1,5 +1,5 @@
 class AddColumnUnitOfMeasureForRequisitionItem < ActiveRecord::Migration[5.0]
   def change
-  	add_column :requisition_items, :unit_of_measure_id, :integer
+  	add_reference :requisition_items, :unit_of_measures, foreign_key: true
   end
 end
