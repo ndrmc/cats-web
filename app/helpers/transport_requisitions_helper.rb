e27f9f820874e147e5dcc422247428e8dc5a4339
Nathnael Getahun <contact.nathnael@gmail.com>
2017-08-23 10:46:01 +0300

    new-tr Optimized db callbacks and added a loading gif on add tr pop-up

--- app/helpers/transport_requisitions_helper.rb
+++ app/helpers/transport_requisitions_helper.rb
@@ -1,18 +1,19 @@
 module TransportRequisitionsHelper
 
 	def generate_tr (transport_requisition_params)
-		@transport_requisition = TransportRequisition.new(transport_requisition_params)
-		@transport_requisition.save
-		count = Requisition.joins(:requisition_items).where({:operation_id => @transport_requisition.operation_id, :region_id => @transport_requisition.location_id, :status => :approved}).count
-		if count > 0
+		
+		@requisitions = Requisition.joins(:requisition_items).where({:operation_id =>transport_requisition_params[:operation_id], :region_id => transport_requisition_params[:location_id], :status => :approved})
+		if @requisitions.count > 0
+			@transport_requisition = TransportRequisition.new(transport_requisition_params)
+				@transport_requisition.save
 		  	@program = Program.find(Operation.find(@transport_requisition.operation_id).program_id)
 		  	@transport_requisition.reference_number = @program.code.to_s + '/' + @transport_requisition.location_id.to_s + '/' + @transport_requisition.id.to_s + '/' + Time.now.year.to_s
 		  	@transport_requisition.status = :open
 		  	@transport_requisition.save
 
-		  	Requisition.where({:operation_id => @transport_requisition.operation_id, :region_id => @transport_requisition.location_id, :status => :approved}).find_each do |requisition|
+		  	@requisitions.find_each do |requisition|
 
-		  		RequisitionItem.where({:requisition_id => requisition.id})
+		  		requisition.requisition_items
 				.find_each do |ri|
 			  		TransportRequisitionItem.new( transport_requisition_id: @transport_requisition.id, requisition_id: requisition.id, fdp_id: ri.fdp_id, commodity_id: requisition.commodity_id, quantity: ri.amount, has_transport_order: false, created_by: current_user.id).save
 			  	end
@@ -21,7 +22,6 @@ module TransportRequisitionsHelper
 		  	end
 		  	return true
 	    else
-	    	@transport_requisition.destroy
 	    	return false
 	    end
   	end
