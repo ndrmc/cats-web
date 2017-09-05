990231d8bd049f7f6af7ac259fee96b4191a746f
Nathnael Getahun <contact.nathnael@gmail.com>
2017-09-05 09:08:19 +0300

    Added a check if contract is signed before creating TO

--- app/helpers/transport_orders_helper.rb
+++ app/helpers/transport_orders_helper.rb
@@ -1,5 +1,9 @@
 module TransportOrdersHelper
 	def generate_transport_order (tr_id, bid_id)
+		@contract = Contract.where(:transporter_id => tr_id, :bid_id => bid_id, :signed => true)&.first
+		if (! @contract.present?)
+			return false
+		end
 		@transport_requisition = TransportRequisition.find(tr_id)
 		@uom = UnitOfMeasure.where(:code => 'MT').first
 		@transport_requisition.transport_requisition_items.where(:has_transport_order => false)
@@ -17,7 +21,7 @@ module TransportOrdersHelper
 					tr_item.has_transport_order = true
 					tr_item.save
 				else
-					@new_to = TransportOrder.new(order_no: SecureRandom.uuid, transporter_id: bid_quotation.transporter_id, bid_id: bid_id, operation_id: @transport_requisition.operation_id, region_id: @woreda.parent.parent_node_id, order_date: Time.current, created_date: Time.current, start_date: 3.days.from_now, end_date: 13.days.from_now, printed_copies: 0, status: 0, created_by: current_user.id, transport_requisition_id: @transport_requisition.id)
+					@new_to = TransportOrder.new(order_no: SecureRandom.uuid, contract_id: @contract.id, transporter_id: bid_quotation.transporter_id, bid_id: bid_id, operation_id: @transport_requisition.operation_id, region_id: @woreda.parent.parent_node_id, order_date: Time.current, created_date: Time.current, start_date: 3.days.from_now, end_date: 13.days.from_now, printed_copies: 0, status: 0, created_by: current_user.id, transport_requisition_id: @transport_requisition.id)
 					@new_to.save
 					@new_to.order_no = "TRN-ORD-" + @new_to.id.to_s	
 					@new_to.save				
