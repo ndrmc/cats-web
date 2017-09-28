c821092cb85c069a88e6134b7101ff20ad5ed825
Nathnael Getahun <contact.nathnael@gmail.com>
2017-09-28 09:39:37 +0300

    Moved some dispath logic to model

--- app/controllers/dispatches_controller.rb
+++ app/controllers/dispatches_controller.rb
@@ -29,49 +29,9 @@ class DispatchesController < ApplicationController
             
             gin_filter = {:'dispatches.hub_id' => params[:hub], :'dispatches.operation_id' => params[:operation], :'dispatches.fdp_id' => @fdp_ids }
 
-            @dispatch_summary = []
-            
-            Requisition.joins(:requisition_items, :commodity).where( allocation_filter ).where(' requisition_items.amount > 0').uniq{|t| t.requisition_no }.each do |allocation|
-                @row = Hash.new
-                @row['requisition_no'] = allocation.requisition_no
-                @row['commodity'] = allocation.commodity.name
-                @total_allocated = 0
-                allocation.requisition_items.where(:fdp_id => @fdp_ids).each do |ri|
-                    uom_id = allocation&.operation&.ration&.ration_items.where(commodity_id: allocation.commodity_id)&.first&.unit_of_measure_id
-                    if(uom_id.present?)
-                        @total_allocated = @total_allocated + UnitOfMeasure.find(uom_id).to_ref(ri.amount)
-                    else
-                        @total_allocated = @total_allocated + ri.amount
-                    end
-                end
-                @row['allocated'] = @total_allocated
-                # dispatch_filter[:requisition_number] = allocation.requisition_no
-                @total_dispatched = 0
-                Dispatch.joins(:dispatch_items).where( {:'dispatches.hub_id' => params[:hub], :'dispatches.operation_id' => params[:operation], :'dispatches.fdp_id' => @fdp_ids, :'dispatches.requisition_number' => allocation.requisition_no } ).select(:id, :'dispatch_items.quantity', :'dispatch_items.unit_of_measure_id').find_each do |di|                    
-                    @qty_in_ref = UnitOfMeasure.find(di.unit_of_measure_id).to_ref(di.quantity)
-                    @total_dispatched = @total_dispatched + @qty_in_ref
-                end
-                @row['dispatched'] = @total_dispatched
-                @row['progress'] = (@total_dispatched.to_f / @total_allocated.to_f) * 100
-                @dispatch_summary << @row
-            end
+            @dispatch_summary = Dispatch.fdp_allocations(params[:hub], params[:operation], allocation_filter, @fdp_ids)                       
 
-            @dispatches = []
-            Dispatch.joins( :transporter, dispatch_items: [:unit_of_measure, :commodity] ).where( dispatch_filter ).find_each do |dispatch|
-                @gin_row = Hash.new
-                @gin_row['gin_no'] = dispatch.gin_no
-                @gin_qty = 0
-                dispatch.dispatch_items.find_each do |di|
-                    @qty_in_ref = UnitOfMeasure.find(di.unit_of_measure_id).to_ref(di.quantity)
-                    @gin_qty = @gin_qty + @qty_in_ref
-                    @gin_row['commodity'] = di.commodity.name
-                end
-                @gin_row['dispatch_qty'] = @gin_qty
-                @gin_row['uom'] = 'MT'
-                @gin_row['transporter'] = dispatch.transporter.name
-                @gin_row['dispatch_date'] = dispatch.dispatch_date
-                @dispatches << @gin_row
-            end
+            @dispatches = Dispatch.fdp_dispatches(dispatch_filter)           
             
         else 
             @dispatch_summary = []
