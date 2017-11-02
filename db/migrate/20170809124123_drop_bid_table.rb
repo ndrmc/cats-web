a29c8a4d11ed2e247741338a2ad2da8975f05b37
dawitd <dawitdb@yahoo.com>
2017-11-02 13:42:00 +0300

    Update 20170809124123_drop_bid_table.rb

--- db/migrate/20170809124123_drop_bid_table.rb
+++ db/migrate/20170809124123_drop_bid_table.rb
@@ -1,9 +1,10 @@
 class DropBidTable < ActiveRecord::Migration[5.0]
   def change
+    if table_exists?("bids")
       drop_table :bids do |bid|
           bid.string :bid_no
           bid.timestamps
       end
-      
+    end
   end 
 end
