b1d751161025b64a9e90914ac6188dc442ad6320
Nathnael Getahun <contact.nathnael@gmail.com>
2017-11-27 12:14:31 +0300

    Added unit converion to psnp plan report

--- app/pdfs/psnp_pdf.rb
+++ app/pdfs/psnp_pdf.rb
@@ -26,13 +26,16 @@ end
         dynamic_data = []
         dynamic_data = ["Region","Beneficiary Number"]
         @psnp.ration.ration_items.each do |item|
-        dynamic_data << item.commodity.name
+        dynamic_data << item.commodity.name + ' (QTL)'
         end
             [dynamic_data] +
             @psnp_by_region.map do |item_by_region|
             commmodities = []
             @psnp.ration.ration_items.each do |item|
-                commmodities << item.amount * item_by_region.total_beneficiaries
+                target_unit = UnitOfMeasure.find_by(name: "Quintal")
+                current_unit = UnitOfMeasure.find(item.unit_of_measure_id)
+                amount_in_qtl = target_unit.convert_to(current_unit.name, item.amount)
+                commmodities << (amount_in_qtl * item_by_region.total_beneficiaries).round(2)
             end
         
              [@location.find(item_by_region.region_id).name,item_by_region.total_beneficiaries] + commmodities    
