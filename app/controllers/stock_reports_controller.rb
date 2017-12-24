ee6689c56b5519df35f8934a753e6873154c6564
Yared Ayalew <yareda@gmail.com>
2017-12-24 10:00:16 +0300

    Add stock report controller

---
+++ app/controllers/stock_reports_controller.rb
@@ -0,0 +1,6 @@
+class StockReportsController < ApplicationController
+
+    def index 
+        
+    end
+end
---
+++ test/controllers/stock_reports_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StockReportsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
