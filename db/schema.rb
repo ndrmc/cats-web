8db0b65693878fad526e1c218b4fa9a9806827ae
Nathnael Getahun <contact.nathnael@gmail.com>
2018-01-03 14:32:08 +0300

    Added balance unit on ui

--- db/schema.rb
+++ db/schema.rb
@@ -954,6 +954,7 @@ ActiveRecord::Schema.define(version: 20180103083542) do
     t.integer  "receipt_id"
     t.integer  "commodity_category_id"
     t.integer  "commodity_id"
+    t.decimal  "quantity",                         precision: 15, scale: 2
     t.integer  "project_id"
     t.integer  "created_by"
     t.integer  "modified_by"
@@ -964,7 +965,6 @@ ActiveRecord::Schema.define(version: 20180103083542) do
     t.integer  "unit_of_measure_id"
     t.string   "receive_id",            limit: 36,                                          null: false
     t.string   "receive_item_id",       limit: 36,                                          null: false
-    t.decimal  "quantity",                         precision: 15, scale: 2
     t.index ["commodity_category_id"], name: "index_receipt_lines_on_commodity_category_id", using: :btree
     t.index ["commodity_id"], name: "index_receipt_lines_on_commodity_id", using: :btree
     t.index ["project_id"], name: "index_receipt_lines_on_project_id", using: :btree
@@ -1068,15 +1068,14 @@ ActiveRecord::Schema.define(version: 20180103083542) do
   create_table "requisition_items", force: :cascade do |t|
     t.integer  "requisition_id"
     t.integer  "fdp_id"
-    t.integer  "beneficiary_no",     null: false
-    t.decimal  "amount",             null: false
+    t.integer  "beneficiary_no", null: false
+    t.decimal  "amount",         null: false
     t.decimal  "contingency"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.integer  "created_by"
     t.integer  "modified_by"
     t.datetime "deleted_at"
-    t.integer  "unit_of_measure_id"
     t.index ["deleted_at"], name: "index_requisition_items_on_deleted_at", using: :btree
   end
 
