013456a2b6463326017f28daa2e9eff7f098a291
dawit <dawitdb@yahoo.com>
2018-01-09 17:01:44 +0300

    fix failed tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   
  
   
-  get '/stock_movements/getCommodity/:id', to: 'stock_movements#getCommodity'
-  resources :stock_movements
+
   get '/warehouse_allocations/warehouse_allocation_zonal_view'
   get '/warehouse_allocations/warehouse_allocation_fdp_view'
   resources :warehouse_allocations
@@ -20,6 +19,9 @@ Rails.application.routes.draw do
   get 'fdp_operation_summary/index'
 
  scope "(:locale)", locale: /en|am/ do
+  get '/stock_movements/getCommodity/:id', to: 'stock_movements#getCommodity'
+  resources :stock_movements
+
   get '/transport_requisitions/print', to: 'transport_requisitions#print'
   get '/transport_requisitions/get_fdps_list', to: 'transport_requisitions#get_fdps_list'
   post '/transport_requisitions/create_to_for_exceptions', to: 'transport_requisitions#create_to_for_exceptions'
--- test/controllers/stock_movements_controller_test.rb
+++ test/controllers/stock_movements_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class StockMovementsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
+    sign_in users(:admin)
     @stock_movement = stock_movements(:one)
   end
 
@@ -17,30 +20,30 @@ class StockMovementsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create stock_movement" do
     assert_difference('StockMovement.count') do
-      post stock_movements_url, params: { stock_movement: { commodity_id: @stock_movement.commodity_id, description: @stock_movement.description, destination_hub_id: @stock_movement.destination_hub_id, destination_store_id: @stock_movement.destination_store_id, destination_warehouse_id: @stock_movement.destination_warehouse_id, project_id: @stock_movement.project_id, quantity: @stock_movement.quantity, quantity: @stock_movement.quantity, source_hub_id: @stock_movement.source_hub_id, source_store_id: @stock_movement.source_store_id, source_warehouse_id: @stock_movement.source_warehouse_id } }
+      post stock_movements_url, params: { stock_movement: { commodity_id: @stock_movement.commodity_id, description: @stock_movement.description, destination_hub_id: @stock_movement.destination_hub_id, destination_store_id: @stock_movement.destination_store_id, destination_warehouse_id: @stock_movement.destination_warehouse_id, project_id: @stock_movement.project_id, quantity: @stock_movement.quantity, source_hub_id: @stock_movement.source_hub_id, source_store_id: @stock_movement.source_store_id, source_warehouse_id: @stock_movement.source_warehouse_id } }
     end
 
     assert_redirected_to stock_movement_url(StockMovement.last)
   end
 
   test "should show stock_movement" do
-    get stock_movement_url(@stock_movement)
+    get stock_movement_url("en",@stock_movement)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_stock_movement_url(@stock_movement)
+    get edit_stock_movement_url("en",@stock_movement)
     assert_response :success
   end
 
   test "should update stock_movement" do
-    patch stock_movement_url(@stock_movement), params: { stock_movement: { commodity_id: @stock_movement.commodity_id, description: @stock_movement.description, destination_hub_id: @stock_movement.destination_hub_id, destination_store_id: @stock_movement.destination_store_id, destination_warehouse_id: @stock_movement.destination_warehouse_id, project_id: @stock_movement.project_id, quantity: @stock_movement.quantity, quantity: @stock_movement.quantity, source_hub_id: @stock_movement.source_hub_id, source_store_id: @stock_movement.source_store_id, source_warehouse_id: @stock_movement.source_warehouse_id } }
+    patch stock_movement_url("en",@stock_movement), params: { stock_movement: { commodity_id: @stock_movement.commodity_id, description: @stock_movement.description, destination_hub_id: @stock_movement.destination_hub_id, destination_store_id: @stock_movement.destination_store_id, destination_warehouse_id: @stock_movement.destination_warehouse_id, project_id: @stock_movement.project_id, quantity: @stock_movement.quantity, source_hub_id: @stock_movement.source_hub_id, source_store_id: @stock_movement.source_store_id, source_warehouse_id: @stock_movement.source_warehouse_id } }
     assert_redirected_to stock_movement_url(@stock_movement)
   end
 
   test "should destroy stock_movement" do
     assert_difference('StockMovement.count', -1) do
-      delete stock_movement_url(@stock_movement)
+      delete stock_movement_url("en",@stock_movement)
     end
 
     assert_redirected_to stock_movements_url
