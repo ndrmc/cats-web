6948d0dc6f56156e0b4bf61a744e53f03d85d5be
dawitd <dawitdb@yahoo.com>
2018-02-09 11:23:09 +0300

    Update gift_certificate_pdf.rb

--- app/pdfs/gift_certificate_pdf.rb
+++ app/pdfs/gift_certificate_pdf.rb
@@ -1,11 +1,15 @@
 require 'prawn/table'
 class GiftCertificatePdf < PdfReport
-    def initialize(gift,organization,commodity,currency,date_min,date_max)
+    def initialize(gift,organization,commodity,currency,date_min,date_max,toal_weight_in_MT,toal_estimated_price_in_usd,toal_estimated_price_in_birr,total_estimated_tax)
         super top_margin: 50, :page_size => "A4", :page_layout => :landscape
        @gift = gift
        @organization = organization
        @commodity = commodity
        @currency = currency
+       @total_weight_in_MT = toal_weight_in_MT
+       @toal_estimated_price_in_usd = toal_estimated_price_in_usd
+       @toal_estimated_price_in_birr=toal_estimated_price_in_birr
+       @total_estimated_tax =  total_estimated_tax
         header "Gift certificate from " + date_min.to_s + " to " + date_max.to_s
         gift_certificate
         footer "Commodity Allocation and Tracking System"
@@ -20,6 +24,12 @@ def gift_certificate
       self.row_colors = ["DDDDDD", "FFFFFF"]
       self.header = true
     end
+     move_down 10
+           text "Total Weight in MT: " + ActiveSupport::NumberHelper.number_to_currency(@total_weight_in_MT.to_s,precision: 2, :unit=> '')
+           text "Total Estimated Price in USD:" +  ActiveSupport::NumberHelper.number_to_currency(@toal_estimated_price_in_usd.to_s, precision: 2)
+           text "Total Estimated price in Birr:" +  ActiveSupport::NumberHelper.number_to_currency(@toal_estimated_price_in_birr.to_s, precision: 2,:unit=> 'ETB ')
+           text "Total Estimated Tax: " +  ActiveSupport::NumberHelper.number_to_currency(@total_estimated_tax.to_s, precision: 2,:unit=> 'ETB ')
+        
 end
 
 end
