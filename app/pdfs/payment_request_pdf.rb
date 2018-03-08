591155b8fe7650689cd0eca914ff5f614b5a0180
dawit <dawitdb@yahoo.com>
2018-03-08 16:46:54 +0300

    add payment request attachment

--- app/pdfs/payment_request_pdf.rb
+++ app/pdfs/payment_request_pdf.rb
@@ -1,9 +1,14 @@
 class PaymentRequestPdf  < PdfReport
-        def initialize(payment_requested)
+        def initialize(payment_requested, dispatched, received,freight_charge,transporter, current_user)
         super(top_margin: 50)
         @payment_requested = payment_requested
-       
-        header "# Program \t\t-\t\t Allocation for #{}"
+        @dispatched = dispatched
+        @received = received
+        @freight_charge = freight_charge
+        @transporter = transporter
+        @user =  current_user
+        header "Payment Request for transporter: " + "#{@transporter}"
+        
         payment_request
         
         footer "Commodity Allocation and Tracking System"
@@ -20,28 +25,31 @@ class PaymentRequestPdf  < PdfReport
         move_down  10
         text "Total allocated: " 
         move_down 1
-        text "Total dispatched: "
+        text "Total dispatched: " + @dispatched.to_s
         move_down 1
-        text "Total delivered: " 
+        text "Total delivered: " + @received.to_s
         move_down 1
-        text "Total Freight Charge: " 
+        text "Total Loss: " 
+        move_down 1
+        text "Total Freight Charge: " + @freight_charge.to_s
         text "\n"
         text "\n"
         text "\n"
-        text "Prepared by: ...................................................    Certified by: ..................................................."
+        text "Prepared by: " + "#{@user}" 
+        text "Certified by: ..................................................."
         end
     end
-
+    
     def payment_request_items
+        @count = 0
         dynamic_data = []
-        dynamic_data = ["Item","Reference No","Req.No","G.R.N","Commodity", "Source","Destination","Received Qty","Tariff","Freight Chanrge"]
+        dynamic_data = ["Item","Reference No","Req.No","G.R.N","Commodity", "Source","Destination","Received Qty","Tariff","Loss","Freight Chanrge"]
         [dynamic_data] +
        @payment_requested.map do |detail|
-                     ["",detail&.payment_request&.reference_no, detail&.requisition_no,detail&.grn_no,detail&.commodity&.name,detail&.hub&.name, detail&.fdp&.name, detail&.received,detail&.tariff, detail&.freightCharge ]     
-              
-
-             
+             @count =+1
+                     [@count,detail&.payment_request&.reference_no, detail&.requisition_no,detail&.grn_no,detail&.commodity&.name,detail&.hub&.name, detail&.fdp&.name, detail&.received,detail&.tariff, "",detail&.freightCharge ]
         end
+       
     end   
 end
 
