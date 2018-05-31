15caf090297437a443bc771eef7e51476d1a43d1
dawit <dawitdb@yahoo.com>
2018-05-31 17:46:03 +0300

    fix layout issue

--- app/pdfs/transport_order_pdf.rb
+++ app/pdfs/transport_order_pdf.rb
@@ -6,12 +6,13 @@ class TransportOrderPdf < PdfReport
         @transport_order = transport_order
         @transport_order_items = transport_order_items
         @zones = zones
-         @region = region
+        @region = region
         @commodities = commodities
         @requisitions = requisitions
         @references = references
         donor = []
-       
+        @A4_SIZE = 100.freeze
+        @operation = Operation.find_by(id: @transport_order.operation_id)&.name
          @transport_order_items.map do |item|
             requisition_id = Requisition.find_by(requisition_no: item.requisition_no)&.id
              project_id = ProjectCodeAllocation.where(requisition_id: requisition_id).limit(1).pluck(:project_id)
@@ -23,12 +24,15 @@ class TransportOrderPdf < PdfReport
                 end
              end
         end
-        header "Transport Order"
+         header "Transport Order"
+         move_down 10
+        bounding_box([bounds.left, bounds.top -  100 ], :width => bounds.width, :height => bounds.height - 210) do
+       
         text "<b>Order No.</b> <u> #{@transport_order.order_no}</u>", :align => :center, :inline_format => true
         text "\n\n<b>I. <u>TRANSACTION DETAILS</u></b>", :inline_format => true
         text "Contract Number: #{@transport_order&.contract&.contract_no}" , :align => :right
         text "Transpot Order Date: #{@transport_order&.order_date}", :align => :right
-        
+       
         t = [
              
                  ["Transporter:","#{@transport_order&.transporter&.name}"],
@@ -37,20 +41,28 @@ class TransportOrderPdf < PdfReport
                  ["Commodity:","#{@commodities.to_s}", "  " * 3 ,"Bid Document No:","#{@transport_order&.bid&.bid_number}"],
                  ["Donor:", "#{donor.to_s}", "  " * 2 ,"Performance Bond Receipt #","#{@transport_order&.performance_bond_receipt}"],
                  ["RequisitionNo:","#{@requisitions.to_s}", "  " * 2 ,"Transport Expiry Date:","#{@transport_order&.end_date}"],
+                 ["Operation:","#{ @operation }"],
                  ["Reference:","#{ @references.to_s}"]
         ]
-        table(t, :cell_style => {:border_width => 0})   
+        table(t, :column_widths => (@A4_SIZE), :cell_style => {:border_width => 0})   
          
 
         text "\n<b>II. <u>COMMODITY DETAILS</u></b>", :inline_format => true
         transport_orders
-        
+         
+            text "\n\n\n<b>III. <u>APPROVING CERTIFICATE</u></b>", :inline_format => true
+            text "\n<b>For Consigner                                                                           For Transporting Agency</b>", :inline_format => true
+            text "Name: __________________________                                    Name: ________________________", :inline_format => true
+            text "Signature: _____________________                                        Signature: ___________________", :inline_format => true
+            text "Date: __________________________                                      Date: ________________________", :inline_format => true
+    end
+
         footer "Commodity Allocation and Tracking System\nCopy Distribution\nOriginal:- Consignee; 1st Copy:- Carrier; 2nd Copy:- Finance Services; 3rd Copy:- Legal Services; 4th Copy:- Retained as Copy"
         
     end
 
     def transport_orders
-         bounding_box([bounds.left, bounds.top - 315 ], :width => bounds.width, :height => bounds.height - 400) do
+        
         table transport_order_items do
         row(0).font_style = :bold
         column(0).width = 40
@@ -59,14 +71,10 @@ class TransportOrderPdf < PdfReport
         column(7).width = 60
         self.row_colors = ["DDDDDD", "FFFFFF"]
         self.header = true
-    end
-    text "\n\n\n<b>III. <u>APPROVING CERTIFICATE</u></b>", :inline_format => true
-        text "\n<b>For Consigner                                                                           For Transporting Agency</b>", :inline_format => true
-        text "Name: __________________________                                    Name: ________________________", :inline_format => true
-        text "Signature: _____________________                                        Signature: ___________________", :inline_format => true
-        text "Date: __________________________                                      Date: ________________________", :inline_format => true
-    end
-    end
+    
+end
+
+   end 
 
     def transport_order_items
         dynamic_data = []
