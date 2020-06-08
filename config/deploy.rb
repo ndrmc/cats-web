5eda196db3abd49b070b876da5b637b9fba5160a
Nathnael Getahun <contact.nathnael@gmail.com>
2020-06-09 01:48:05 +0300

    Changed ruby deploy version to 2.5.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    1
 
 # rbenv and ruby settings
-set :rbenv_ruby, "2.5.0"
+set :rbenv_ruby, "2.7.1"
 
 # Don't change these unless you know what you're doing
 set :pty,             true
